8f88b7384d270266c111dbe7864722e2f5c80052
Iris Souza <iriscsouza2@gmail.com>
2025-04-01 18:03:32 -0300

    PC Mania - Exercício C06

--- PC Mania/src/Main.java
+++ PC Mania/src/Main.java
@@ -6,27 +6,27 @@ public class Main {
         //Promoção 1
 
         Computador c1 = new Computador();
-        c1.marca = "Apple";
+        c1.marca = "Apple ";
         c1.preco = 134;
 
         MemoriaUSB m1 = new MemoriaUSB();
-        m1.nome = "Pen-Drive";
+        m1.nome = "Pen-Drive ";
         m1.capacidade = 16;
 
 
-        c1.s1.nome = "Linux Ubuntu";
+        c1.s1.nome = "Linux Ubuntu ";
         c1.s1.tipo = 32;
 
         HardwareBasico h1 = new HardwareBasico();
-        h1.nome = "Pentium Core i3";
+        h1.nome = "Pentium Core i3 ";
         h1.capacidade = 2200;
 
         HardwareBasico h2 = new HardwareBasico();
-        h2.nome = "Memória Ram";
+        h2.nome = "Memória Ram ";
         h2.capacidade = 8;
 
         HardwareBasico h3 = new HardwareBasico();
-        h3.nome = "HD";
+        h3.nome = "HD ";
         h3.capacidade = 500;
 
         //add no vetor
@@ -34,33 +34,33 @@ public class Main {
         c1.hardwares[1] = h2;
         c1.hardwares[2] = h3;
 
-        c1.mostraPCConfigs();
+
 
 
         //Promoção 2
 
         Computador c2 = new Computador();
-        c2.marca = "Samsung";
+        c2.marca = "Samsung ";
         c2.preco = 1368;
 
         MemoriaUSB m2 = new MemoriaUSB();
-        m2.nome = "Pen-Drive";
+        m2.nome = "Pen-Drive ";
         m2.capacidade = 32;
 
 
-        c2.s1.nome = "Windows 8";
+        c2.s1.nome = "Windows 8 ";
         c2.s1.tipo = 64;
 
         HardwareBasico h4 = new HardwareBasico();
-        h4.nome = "Pentium Core i5";
+        h4.nome = "Pentium Core i5 ";
         h4.capacidade = 3370;
 
         HardwareBasico h5 = new HardwareBasico();
-        h5.nome = "Memória Ram";
+        h5.nome = "Memória Ram ";
         h5.capacidade = 16;
 
         HardwareBasico h6 = new HardwareBasico();
-        h6.nome = "HD";
+        h6.nome = "HD ";
         h6.capacidade = 1;
 
         //add no vetor
@@ -68,31 +68,31 @@ public class Main {
         c2.hardwares[1] = h5;
         c2.hardwares[2] = h6;
 
-        c2.mostraPCConfigs();
+
 
         //Promoção 3
 
         Computador c3 = new Computador();
-        c3.marca = "Dell";
+        c3.marca = "Dell ";
         c3.preco = 5812;
 
         MemoriaUSB m3 = new MemoriaUSB();
-        m3.nome = "HD Externo";
+        m3.nome = "HD Externo ";
         m3.capacidade = 1;
 
-        c3.s1.nome = "Windows 10";
+        c3.s1.nome = "Windows 10 ";
         c3.s1.tipo = 64;
 
         HardwareBasico h7 = new HardwareBasico();
-        h7.nome = "Pentium Core i7";
+        h7.nome = "Pentium Core i7 ";
         h7.capacidade = 4500;
 
         HardwareBasico h8 = new HardwareBasico();
-        h8.nome = "Memória Ram";
+        h8.nome = "Memória Ram ";
         h8.capacidade = 32;
 
         HardwareBasico h9 = new HardwareBasico();
-        h9.nome = "HD";
+        h9.nome = "HD ";
         h9.capacidade = 2;
 
         //add no vetor
@@ -100,20 +100,23 @@ public class Main {
         c3.hardwares[1] = h2;
         c3.hardwares[2] = h9;
 
-        c3.mostraPCConfigs();
+
 
         //Cliente
 
-        Cliente cliente = new Cliente("Iris", 123456789);
+        Cliente cliente = new Cliente("Iris Souza", 123456789);
             int x;
             Scanner sc = new Scanner(System.in);
             int y;
 
 
         System.out.println("Bem vindo à PC Mania!");
-        System.out.println("Promoções disponíveis: ");
+        System.out.println("Estas são nossas promoções disponíveis: \n");
+        System.out.println("Promoção 1:");
         c1.mostraPCConfigs();
+        System.out.println("Promoção 2:");
         c2.mostraPCConfigs();
+        System.out.println("Promoção 3:");
         c3.mostraPCConfigs();
 
 
@@ -149,14 +152,14 @@ public class Main {
             }
 
             else if (x == 0){
-                System.out.println("Obrigada pela compra!");
+                System.out.println("Agradecemos pela compra!");
             }
 
 
         }while (x != 0);
-        System.out.println("Total da compra do cliente " + cliente.nome + " de cpf " + cliente.cpf + " é de: R$" +  cliente.calculaTotalCompra() );
+        System.out.println("Total da compra do cliente " + cliente.nome + " CPF n° " + cliente.cpf + " é de: R$ " +  cliente.calculaTotalCompra() );
 
-        System.out.println("As configurações dos PCs comprados são: 1");
+        System.out.println("As configurações dos PCs comprados são: ");
         for (int i = 0; i < cliente.computadores.length; i++) {
             if (cliente.computadores[i] != null) {
                 cliente.computadores[i].mostraPCConfigs();
